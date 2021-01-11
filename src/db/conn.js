b1dff4afe4eb5b24f7a3f4f2bb9c100c3eaceab1
Bhupinder <bhupindersingh987686@gmail.com>
2021-01-11 14:42:03 +0530

    Fifth commit

--- src/db/conn.js
+++ src/db/conn.js
@@ -5,6 +5,6 @@ const mongoose = require("mongoose");           // with mongoose object we will 
 
 // Create connection  (connect() returns promises) 
 // Note : Three stages in promises (i) pending       (ii) resolve        (iii) reject
-mongoose.connect("mongodb+srv://bhupinder:<sandeepB0@>@cluster.miexj.mongodb.net/<dbname>?retryWrites=true&w=majority", { useNewUrlParser: true, useUnifiedTopology : true, useCreateIndex : true})
+mongoose.connect("mongodb+srv://bhupinder:sandeepB0@@cluster.miexj.mongodb.net/cl?retryWrites=true&w=majority", { useNewUrlParser: true, useUnifiedTopology : true, useCreateIndex : true})
 .then( () => console.log("Connection successfull ......."))
 .catch( (err) => console.log(err)); 
